c49b4341b65e675f199a71dcde2b1ece0d376c39
Choe <yeongseon.choe@sap.com>
2019-07-09 17:37:56 +0900

    2-Project-Structure

---
+++ 2-Project-Structure/src/calculator.py
@@ -0,0 +1,15 @@
+
+def add(x, y):
+    return x + y
+
+
+def substract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
---
+++ 2-Project-Structure/tests/test_calculator.py
@@ -0,0 +1,5 @@
+from src.calculator import add
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(2, 2) == 3
